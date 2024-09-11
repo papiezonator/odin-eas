1074be5397cd26d5177201c07b53bcf819a58b93
papiezonator <52249311+papiezonator@users.noreply.github.com>
2024-09-11 22:36:47 +0200

    modified script to add classes

--- script.js
+++ script.js
@@ -4,15 +4,28 @@ let container = document.querySelector("#container");
 
 document.addEventListener("DOMContentLoaded", () =>{
     for(let i = 0; i < 16; i++){
-        let breakPoint = document.createElement("div");
-        container.appendChild(breakPoint);
-        for(let i = 0; i < 16; i++){
+        let column = document.createElement("div");
+        container.appendChild(column);
+
+        column.classList.add("column");
+
+        //document.getElementsByClassName("column")[i].style.border = "2px black solid";
+
+        
+
+        for(let x = 0; x < 16; x++){
             let gridElement = document.createElement("div");
-            breakPoint.appendChild(gridElement);
+            column.appendChild(gridElement);
             gridElement.textContent = "Mario";
             
+            gridElement.classList.add("grid");
+
+            //document.getElementsByClassName("grid")[x].style.border = "2px black solid";
+
         }
         
     }
+
+    
 
 })
