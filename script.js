849465dedba65d59b949cf8385cb0099613060a3
papiezonator <52249311+papiezonator@users.noreply.github.com>
2024-09-15 19:49:16 +0200

    made createGrid a separate function, added code for canvas reloading, further improvements needed

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 
-let container = document.querySelector("#container");
+const container = document.querySelector("#container");
 
-
-document.addEventListener("DOMContentLoaded", () =>{
-    for(let i = 0; i < 16; i++){
+function createGrid(columns, gridElements){
+    for(let i = 0; i < columns; i++){
         let column = document.createElement("div");
         container.appendChild(column);
 
@@ -13,20 +12,46 @@ document.addEventListener("DOMContentLoaded", () =>{
 
         
 
-        for(let x = 0; x < 16; x++){
+        for(let x = 0; x < gridElements; x++){
             let gridElement = document.createElement("div");
             column.appendChild(gridElement);
-            gridElement.textContent = "Mario";
+            
             
             gridElement.classList.add("grid");
+            
+            
+            
+            gridElement.style.height = "60px";
+            gridElement.style.width = "60px";
 
             //document.getElementsByClassName("grid")[x].style.border = "2px black solid";
             gridElement.addEventListener("mouseenter", (event) =>
-            {event.target.style.color = "orange"})
+            {event.target.style.backgroundColor = "black"})
         }
         
     }
+}
 
-   
+document.addEventListener("DOMContentLoaded", () =>{
+    createGrid(16,16);
+    
 })
 
+const dimensionButton = document.querySelector("#canvas");
+
+dimensionButton.addEventListener("click", ()=>{
+    containerClearance();
+    
+    let size = prompt("Enter a number: ");
+
+    createGrid(size, size);
+
+    let size2 = 960/size;
+    console.log(`Dimensions are ${size2}px by ${size2}px`)
+})
+
+function containerClearance(){
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+      }
+}
